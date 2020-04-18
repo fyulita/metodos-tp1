9b59b728c69a7885c80030d0fe79778084f31411
Matias <matias.strobl@gmail.com>
2020-04-18 14:53:03 -0300

    Cargo mi funcion Gaussiana como gausss

--- funciones-principales.cpp
+++ funciones-principales.cpp
@@ -22,3 +22,68 @@ void gauss(matrix &M){
         }
     }
 }
+
+
+
+
+
+
+void triangular_matrix(matrix A, vector<double> b,matrix Aaux, vector<double> baux){
+    int n = A.size();
+    for(int i = 0; i < n-1; i++){
+        for(int j = i+1; j < n; j++){
+            double m = A[j][i]/A[i][i];
+            for (int k = i; k < n; k++){
+                double y = -m * A[i][k];
+                double t = A[j][k] + y;
+                Aaux[j][k] = (t - A[j][k]) - y;
+                A[j][k] = t;
+            }
+            double y = -m * b[i];
+            double t = b[j] + y;
+            baux[j] = (t - b[j]) - y;
+            b[j] = t;
+        }
+    }
+
+}
+
+
+
+
+vector<double> gausss(matrix A, vector<double> b){    
+    int n = A.size();
+
+    matrix Aaux;
+    vector<double> baux;
+    for(int i = 0; i < n; i++){  
+        vector<double> row;
+        for(int j = 0; j < n; j++){
+            row.push_back(0);
+        }
+        Aaux.push_back(row);
+        baux.push_back(0);
+    }
+    
+    triangular_matrix(A,b,Aaux,baux);
+    
+    vector<double> resultado;
+
+    for(int i = 0; i < n; i++){
+        resultado.push_back(-1);
+    }
+
+    for(int i = n-1; i >= 0; i--){
+        double res = b[i];
+        double c = 0;
+        for(int j = i+1; j < n; j++){
+            double y = -A[i][j]*resultado[j];
+            double t = res + y;
+            c = (t - res) - y;
+            res = t;
+        }
+        res /= A[i][i];
+        resultado[i] = res;
+    }
+    return resultado;
+}
--- funciones-principales.h
+++ funciones-principales.h
@@ -5,4 +5,9 @@
 
 void gauss(vector<vector<double>> &M);
 
+void triangular_matrix(matrix A, vector<double> b,matrix Aaux, vector<double> baux);
+vector<double> gausss(matrix A, vector<double> b);
+
+
+
 #endif //SOLUCION_FUNCIONES_PRINCIPALES_H

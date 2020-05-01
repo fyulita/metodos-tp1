2c6449c32da71e19e9133e7f4ffd969c78f94b58
stmatias <63887998+stmatias@users.noreply.github.com>
2020-05-01 20:12:04 -0300

    Agrego funcion league

--- funciones-principales.cpp
+++ funciones-principales.cpp
@@ -173,3 +173,28 @@ vector<double> wpatp(int teams, int matches,ifstream& inputFile){
 }
 
 
+vector<double> league(int teams, int matches,ifstream& inputFile){
+    vector<double> matches_played(teams,0);
+    vector<double> wins(teams,0);
+
+    for(int i = 0; i<matches;i++){
+        int date,team1,score,team2,score2;
+        inputFile>>date>>team1>>score>>team2>>score2;
+        if(score>score2){
+            wins[team1-1] += 1;
+        } else{
+            wins[team2-1] +=1;
+        }
+        matches_played[team1-1] +=1;
+        matches_played[team2-1] +=1;
+    }
+    inputFile.close();
+
+    vector<double> res(teams,0);
+    for(int i=0; i<teams; i++){
+        res[i]=wins[i]*3;
+    }
+    return res;
+}
+
+
